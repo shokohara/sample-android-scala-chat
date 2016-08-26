77c55b60c2f9fe51badfabdcb907a6e59ca9de75
shokohara <shokohara@users.noreply.github.com>
2016-08-26 15:07:07 +0900

    Add Crashlytics

--- app/src/main/scala/com/github/skohar/sampleandroidscalachat/MainApplication.scala
+++ app/src/main/scala/com/github/skohar/sampleandroidscalachat/MainApplication.scala
@@ -1,8 +1,14 @@
 package com.github.skohar.sampleandroidscalachat
 
 import android.app.Application
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 
 class MainApplication extends Application {
 
-  override def onCreate() = super.onCreate()
+  override def onCreate() = {
+    super.onCreate()
+    new Fabric.Builder(this).kits(new Crashlytics()).debuggable(true).build()
+    Fabric.`with`(this, new Crashlytics())
+  }
 }
